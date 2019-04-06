801f61a474b07618426407dcd5a2d98f7625ba32
DCsunset <DCsunset@users.noreply.github.com>
2019-04-06 21:23:05 +0800

    Add arrows

--- Enjine/gameCanvas.js
+++ Enjine/gameCanvas.js
@@ -15,7 +15,7 @@ Enjine.GameCanvas.prototype = {
 		this.Canvas = document.getElementById(canvasId);
 
 		this.Canvas.width = window.innerWidth - 5;
-		this.Canvas.height = window.innerHeight - 5 ;
+		this.Canvas.height = window.innerHeight - 5;
 
 		this.Context2D = this.Canvas.getContext("2d");
 		this.BackBuffer = document.createElement("canvas");
@@ -33,10 +33,29 @@ Enjine.GameCanvas.prototype = {
     EndDraw: function() {
         this.Context2D.drawImage(this.BackBuffer, 0, 0, this.BackBuffer.width, this.BackBuffer.height, 0, 0, this.Canvas.width, this.Canvas.height);
 
-		/*
+		var radius = this.Canvas.width / 16;
+
+		var leftImage = new Image();
+		leftImage.src = "images/baseline-arrow_back_invert-24px.svg";
+
+		var rightImage = new Image();
+		rightImage.src = "images/baseline-arrow_forward_invert-24px.svg";
+
+		this.Context2D.save();
+		this.Context2D.globalAlpha = 0.35;
+		this.Context2D.fillStyle = "rgba(0, 255, 255)";
+
 		this.Context2D.beginPath();
-		this.Context2D.arc(this.Canvas.width , this.Canvas.height - 100, 50, 0, 2 * Math.PI);
+		this.Context2D.drawImage(leftImage, this.Canvas.width / 15 - radius, this.Canvas.height / 2 - 2.3 * radius, radius * 2, radius * 2)
+		this.Context2D.arc(this.Canvas.width / 15, this.Canvas.height / 2 - 1.3 * radius, radius, 0, 2 * Math.PI);
 		this.Context2D.fill();
-		*/
+
+		this.Context2D.beginPath();
+		this.Context2D.drawImage(rightImage, this.Canvas.width / 15 * 2 - radius, this.Canvas.height / 2 + 0.3 * radius, radius * 2, radius * 2)
+		this.Context2D.arc(this.Canvas.width / 15 * 2, this.Canvas.height / 2 + 1.3 * radius, radius, 0, 2 * Math.PI);
+		this.Context2D.fill();
+
+		this.Context2D.restore();
+
     }
 };
